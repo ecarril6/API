486dbc028cbc16e2c237e17ca32480f6c53574b6
millerac38 <73674898+millerac38@users.noreply.github.com>
2023-02-20 16:44:10 -0700

    Program to convert images for machine learning process

---
+++ config/api/backend/imageConverter.py
@@ -0,0 +1,36 @@
+from PIL import Image, ImageEnhance, ImageOps
+import matplotlib.pyplot as plt
+import numpy as np
+# import pytesseract
+import cv2
+import os
+
+input_path = "API/config/api/backend/originalImages/"
+dirs = os.listdir(input_path)
+print(dirs)
+
+def imageProcessing():
+    """ Do image processing/convert image and save in new file """
+    for item in dirs:
+        if os.path.isfile(input_path+item):
+            """ Get path of original photos """
+            output_path = "API/config/api/backend/convertedImages/"
+            image = Image.open(input_path+item)
+            f, e = os.path.splitext(output_path+item)
+
+            """ Do image processing tasks """
+            convert_image = image.convert('RGB')
+            image_enhance = ImageEnhance.Contrast(convert_image)
+            contrast = image_enhance.enhance(8)
+
+            # If image needs to be binarized
+            """ Save newly converted image to new folder for ML tasks """
+            contrast.save(f + '.jpg', 'JPEG', quality=100)
+
+imageProcessing()
+
+
+#def invertImage(image_name):
+#    ImageOps.invert(image_name)
+
+#cv2.imshow(invertImage("API/config/api/backend/originalImages/test.png"))
